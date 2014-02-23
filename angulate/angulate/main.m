d44408fd5ea480f8ad981a0b117fa1889a86c223
Jon Rubin <jb.rubin@gmail.com>
2014-02-23 12:44:57 -0500

    Throws some simple CLI arguments into an array for passing to a model class.

--- angulate/angulate/main.m
+++ angulate/angulate/main.m
@@ -13,9 +13,18 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        if (argc < 3 ) {
+            fprintf(stderr, "Please provide at least two points of interest.\n");
+            return 1;
+        }
+        else {
+            NSMutableArray * points = [[NSMutableArray alloc] init];
+            for (int i = 1; i < argc; i++) {
+                NSString * point = [NSString stringWithUTF8String:argv[i]];
+                [points addObject:point];
+            }
+            points = nil;
+        }
     }
     return 0;
 }
