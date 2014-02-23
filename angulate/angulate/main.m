0a8a33117bdbe307d49bfc6ac27edfa27431e3c3
Jon Rubin <jb.rubin@gmail.com>
2014-02-23 12:37:43 -0500

    Adds a CLI project to the workspace, for prototyping the calculations.

---
+++ angulate/angulate/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  angulate
+//
+//  Created by Jonathon Rubin on 2/23/14.
+//  Copyright (c) 2014 Jonathon Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
