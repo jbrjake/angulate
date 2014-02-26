f2b12ebc1892100eb70e0e6c3429871ea5eae997
Jon Rubin <jb.rubin@gmail.com>
2014-02-25 22:40:30 -0500

    Adds a crude debug/test mode that loads some preset points (the centers of St. Pete and Tampa, per Apple Maps).

--- angulate/angulate/main.m
+++ angulate/angulate/main.m
@@ -13,18 +13,23 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+#ifndef DEBUG
         if (argc < 3 ) {
             fprintf(stderr, "Please provide at least two points of interest.\n");
             return 1;
         }
-        else {
-            NSMutableArray * points = [[NSMutableArray alloc] init];
-            for (int i = 1; i < argc; i++) {
-                NSString * point = [NSString stringWithUTF8String:argv[i]];
-                [points addObject:point];
-            }
-            points = nil;
+#endif
+        
+        NSMutableArray * points = [[NSMutableArray alloc] init];
+#ifdef DEBUG
+        [points addObject:@"3400 Central Ave. Saint Petersburg, FL 33711"];
+        [points addObject:@"300 E Kennedy Blvd Tampa, FL 33602"];
+#else
+        for (int i = 1; i < argc; i++) {
+            NSString * point = [NSString stringWithUTF8String:argv[i]];
+            [points addObject:point];
         }
+#endif
     }
     return 0;
 }
